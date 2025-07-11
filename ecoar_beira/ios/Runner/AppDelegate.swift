884709b20c671fe087157cb9fcb6bf36e3d33d7c
Euler-JS <joaoalbertojose96@gmail.com>
2025-07-11 14:44:15 +0200

    Muitas altteracoes

--- ecoar_beira/ios/Runner/AppDelegate.swift
+++ ecoar_beira/ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import Flutter
 import UIKit
-import GoogleMaps
-import AVFoundation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,22 +7,7 @@ import AVFoundation
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY")
     GeneratedPluginRegistrant.register(with: self)
-
-    // Dica: só chame essa função quando for necessário
-    checkCameraPermission()
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func checkCameraPermission() {
-    AVCaptureDevice.requestAccess(for: .video) { granted in
-      if granted {
-        print("Permissão da câmera concedida")
-      } else {
-        print("Permissão da câmera negada")
-      }
-    }
   }
 }
